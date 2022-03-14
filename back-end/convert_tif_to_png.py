6651cbbc79aa27fa9b37136d996fc260e40197f9
MarcAntoineLebourgeois <marc-antoine@happyteams.events>
2022-03-14 10:02:47 +0100

    feat: create images folder in front-folder automatically

--- back-end/convert_tif_to_png.py
+++ back-end/convert_tif_to_png.py
@@ -10,6 +10,15 @@ PATH5 = "../data/kaggle_3m/TCGA_CS_5393_19990606/"
 DEFAULT_PATH = "../front-end/src/images/"
 
 
+def create_images_folder():
+    """Create the folder that will store images in front-end"""
+    # Check whether the specified path exists or not
+    isExist = os.path.exists(DEFAULT_PATH)
+    if not isExist:
+        # Create a new directory because it does not exist
+        os.makedirs(DEFAULT_PATH)
+
+
 def convert_file_in_folder(folder):
     """Conversion function"""
     for infile in os.listdir(folder):
@@ -19,6 +28,7 @@ def convert_file_in_folder(folder):
         cv2.imwrite(DEFAULT_PATH + outfile, read, [int(cv2.IMWRITE_JPEG_QUALITY), 200])
 
 
+create_images_folder()
 convert_file_in_folder(PATH1)
 convert_file_in_folder(PATH2)
 convert_file_in_folder(PATH3)
