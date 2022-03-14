21617045be6bc65b0418fea2e1373f73ac904383
MarcAntoineLebourgeois <marc-antoine@happyteams.events>
2022-03-14 09:35:35 +0100

    feat: improve UX when clicking on image to add points

--- front-end/src/components/PatientImage.tsx
+++ front-end/src/components/PatientImage.tsx
@@ -1,9 +1,22 @@
+import { Typography } from "@mui/material";
 import { FC } from "react";
 import { usePatient } from "../hooks";
 
 export const PatientImage: FC = () => {
-  const { patient, onMouseClick } = usePatient();
+  const { patient, onMouseClick, points } = usePatient();
   const { image } = patient;
   if (!image) return null;
-  return <img src={image} onClick={onMouseClick} />;
+  return (
+    <>
+      <Typography variant="h6">
+        Click on the image below to add points in order to form a contour
+      </Typography>
+      <img src={image} onClick={onMouseClick} />
+      {points.map((point, key) => (
+        <Typography variant="subtitle2" key={key}>
+          x: {point[1]}, y: {point[0]}
+        </Typography>
+      ))}
+    </>
+  );
 };
